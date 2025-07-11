33e679237e67043ff1b21b27e6dadd6bd8a1c0d2
randomplayables <randomplayables@proton.me>
2025-07-11 02:48:50 +0000

    feat: Add src/utils/prime.ts

---
+++ src/utils/prime.ts
@@ -0,0 +1,10 @@
+export function isPrime(num: number): boolean {
+  if (num < 2) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
